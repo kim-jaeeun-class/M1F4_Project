f6be7b79296195b5ea6fa4bbd8f780217789f127
조예주 <dPwn123488@gmail.com>
2025-07-25 15:16:27 +0900

    Revert "250725_커뮤니티(고현지)"
    This reverts commit f2abae1bc7023f8efdda690aa1d3db87d662fddb.

--- project_hyunji/asset/5_community.js
+++
@@ -1,6 +0,0 @@
-const hamburgerBtn = document.getElementById('hamburger-menu');
-const categoryMenu = document.getElementById('category-menu');
-
-hamburgerBtn.addEventListener('click', () => {
-    categoryMenu.classList.toggle('show');
-});
