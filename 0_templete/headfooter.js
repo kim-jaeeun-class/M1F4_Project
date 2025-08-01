a2ed0dbfe2c7d8b044723453a193d3e3116b635b
tis0329 <datascience123@naver.com>
2025-08-01 12:40:20 +0900

    헤드 푸터 수정

--- 0_templete/headfooter.js
+++ 0_templete/headfooter.js
@@ -1,16 +1,23 @@
-//head와 header를 로드하는 자바 스크립트 입니다.
+// 
 
 fetch("../../0_templete/templete.html")
-.then(res => res.text())
-.then(html => {
+  .then(res => res.text())
+  .then(html => {
     const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html'); 
-    //text/html 가상의 문자열 html
+    const doc = parser.parseFromString(html, 'text/html');
 
     const header = doc.querySelector(".head");
     const footer = doc.querySelector(".footer");
 
     if (header) document.querySelector(".head").innerHTML = header.innerHTML;
-    if (footer) document.querySelector(".footer").innerHTML = footer.innerHTML;
-})
-.catch(err => console.error("불러오기 실패:", err));
+    if (footer) {
+      document.querySelector(".footer").innerHTML = footer.innerHTML;
+
+      // header/footer 삽입 후 DOM에 존재할 때 함수 호출
+      requestAnimationFrame(() => {
+        if (typeof bind === "function") bind();
+        if (typeof initChatbot === "function") initChatbot();
+      });
+    }
+  })
+  .catch(err => console.error("불러오기 실패:", err));
